6c21018f391102af7b5da5b21a161d5528bce8f7
Your Name <you@example.com>
2023-01-31 12:19:21 +0530

    add donload media functionality

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
-
+import { saveAs } from 'file-saver'
 
 const Message = ({ isSelf, message, currentUser, slectedChatUser }) => {
+
+  const handleDownload = (fileUrl, filename) => {
+    saveAs(fileUrl, filename)
+  }
 
   return (
 
@@ -19,7 +23,17 @@ const Message = ({ isSelf, message, currentUser, slectedChatUser }) => {
                 message.messageType === "mp3" && <audio src={message.text} controls autoPlay={false} />
               }
               {
-                message.messageType !== "mp3" && < div > {message.text}</div>
+                message.messageType !== "mp3" && message.messageType !== "png" &&
+                < div onClick={() => handleDownload(message.text, message.text.split("/")[message.text.split("/").length - 1])} className="cursor-pointer"> {message.text}</div>
+              }
+              {
+                message.messageType === "png" &&
+                <img
+                  onClick={() => handleDownload(message.text, message.text.split("/")[message.text.split("/").length - 1])}
+                  className="object-cover w-full lg:w-40 lg:h-40"
+                  src={message.text}
+                  alt="image"
+                />
               }
             </div>
           </div>
@@ -38,7 +52,20 @@ const Message = ({ isSelf, message, currentUser, slectedChatUser }) => {
                 message.messageType === "mp3" && <audio src={message.text} controls autoPlay={false} />
               }
               {
-                message.messageType !== "mp3" && < div > {message.text}</div>
+                message.messageType !== "mp3" && message.messageType !== "png" &&
+                < div onClick={() => handleDownload(message.text, message.text.split("/")[message.text.split("/").length - 1])} className="cursor-pointer"> {message.text}</div>
+              }
+
+              {
+                message.messageType === "png" &&
+
+                <img
+                  onClick={() => handleDownload(message.text, message.text.split("/")[message.text.split("/").length - 1])}
+                  className="object-contain w-full lg:w-40 lg:h-30"
+                  src={message.text}
+                  alt="image"
+                />
+
               }
 
             </div>
